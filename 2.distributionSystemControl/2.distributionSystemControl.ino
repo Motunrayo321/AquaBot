3083825c49993628ebec0735e81dfff122a1785c
unknown <samlizzy353@gmail.com>
2024-06-20 15:40:53 -0500

    Added sequence test for both motors, made the control code based on the flow (missing functions, to turn into modules)

--- 2.distributionSystemControl/2.distributionSystemControl.ino
+++ 2.distributionSystemControl/2.distributionSystemControl.ino
@@ -9,21 +9,29 @@ AccelStepper stepperRound(motorInterfaceTypeRound, 8, 9, 10, 11);
 // Define the number of steps per revolution for your stepper motors
 const int stepsPerRevolution = 200;
 
-void setup() {
+int totalDays = 30;
+int pastDays = 1;
+const int dayMilliseconds = 5000;
+
+
+void SetMotorPars() {
   // Set maximum speed and acceleration for stepper 1 and 2
   stepperUp.setMaxSpeed(1000);
   stepperUp.setAcceleration(500);
+  stepperUp.setCurrentPosition(0);
+
   stepperRound.setMaxSpeed(1000);
   stepperRound.setAcceleration(500);
+  stepperRound.setCurrentPosition(0);
   
   // Move stepper 1 a quarter of a revolution and stepper 2 six revolutions
-  stepperUp.moveTo(stepsPerRevolution * 6);  // Six revolutions
-  //stepperUp.setSpeed(800);
+  stepperUp.moveTo(4096 * 2);  // Six revolutions
   stepperRound.moveTo(stepsPerRevolution / 4);  // Quarter revolution
-  //stepperRound.setSpeed(800);
+  stepperRound.setSpeed(500);
 }
 
-void loop() {
+
+void SingleDropper() {
   // Move the steppers to their target positions
   stepperRound.runToPosition();
   delay(2000);
@@ -35,9 +43,27 @@ void loop() {
 
     /* Input code for filling vial */
     
-    delay(5000);
+    delay(3000);
     stepperUp.moveTo(0);
     stepperUp.runToPosition();
   }
+}
 
+
+void setup() {
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  if (pastDays != totalDays) {
+    Serial.print("Starting collection for day "); Serial.println(pastDays);
+    SetMotorPars();
+    SingleDropper();
+    Serial.print("Done for day "); Serial.println(pastDays);
+    delay(dayMilliseconds * pastDays);
+
+    pastDays++;
+  }
+ 
 }
---
+++ 3.ControlInterface/3.ControlInterface.ino
@@ -0,0 +1,117 @@
+#include <AccelStepper.h>
+
+#define motorInterfaceTypeUp 8
+#define motorInterfaceTypeRound 4
+
+AccelStepper stepperUp(motorInterfaceTypeUp, 2, 4, 3, 5);
+AccelStepper stepperRound(motorInterfaceTypeRound, 8, 9, 10, 11);
+
+#define LIMIT_SWITCH_PIN 7
+
+// Define the number of steps per revolution for your stepper motors
+const int stepsPerRevUp = 4096;
+const int stepsPerRevRound = 200;
+
+// Define parameters for keeping count of how many vials have been filled
+int totalDays = 30;
+int bottlesFilled = 1;
+const int dayMilliseconds = 5000;
+
+int rotationAngle = 12;
+int timeToFillTubes = 3000;
+
+
+void SetMotorPars() {
+  // Set maximum speed and acceleration for stepper 1 and 2
+  stepperUp.setMaxSpeed(1000);
+  stepperUp.setAcceleration(500);
+  stepperUp.setCurrentPosition(0);
+
+  stepperRound.setMaxSpeed(1000);
+  stepperRound.setAcceleration(500);
+  stepperRound.setCurrentPosition(0);
+  
+}
+
+
+void SingleDropper() {
+  // Move stepper 1 a quarter of a revolution and stepper 2 six revolutions
+  stepperUp.moveTo(4096 * 2);  // Six revolutions
+  stepperRound.moveTo(stepsToRotate);  // Quarter revolution
+  stepperRound.setSpeed(500);
+
+  // Move the steppers to their target positions
+  stepperRound.runToPosition();
+  delay(2000);
+  stepperUp.runToPosition();
+  
+  // Check if the steppers have reached their target positions
+  if (stepperUp.distanceToGo() == 0 && stepperRound.distanceToGo() == 0) {
+    // Stepper 1 holds position, stepper 2 moves back to the starting position
+
+    /* Input code for filling vial */
+    
+    delay(timeToFillTubes);
+    // FillContainer();
+    stepperUp.moveTo(0);
+    stepperUp.runToPosition();
+  }
+}
+
+
+void HomeRun() {
+  // Move stepper 1 a quarter of a revolution and stepper 2 six revolutions
+  stepperRound.moveTo(stepsPerRevRound * 2);  // Quarter revolution
+  stepperRound.run();
+
+  if (HomeButtonHit) {
+    stepperRound.stop();
+    stepper.setCurrentPosition(0);
+  }
+}
+
+
+bool HomeButtonHit() {
+  if (digitalRead(LIMIT_SWITCH_PIN) == HIGH) {
+    return true; 
+  
+  } else {
+    return false;
+  }
+   
+  delay(100);
+}
+
+
+void PumpFlush() {}
+void PumpControl() {}
+void AirScour() {}
+void FillContainer() {}
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LIMIT_SWITCH_PIN, INPUT);
+
+  int stepsToRotate = (rotationAngle/360) * stepsPerRevRound * bottlesFilled;
+
+}
+
+
+void loop() {
+  if (bottlesFilled != totalDays) {
+    Serial.print("Starting collection for day "); Serial.println(bottlesFilled);
+    // PumpControl();
+    SetMotorPars();
+    HomeRun();
+    // PumpFlush();
+    // AirScour(); // Actual position?
+    SingleDropper();
+    Serial.print("Done for day "); Serial.println(bottlesFilled);
+    delay(dayMilliseconds * bottlesFilled);
+
+
+    bottlesFilled++;
+  }
+ 
+}
